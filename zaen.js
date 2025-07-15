bed06c2d3a5a87d4c7f185e628ef980b12b3dbc1
tabunganku <updataback069@gmail.com>
2025-07-15 21:27:04 +0700

    Update zaen.js

--- zaen.js
+++ zaen.js
@@ -58,6 +58,7 @@ password: "esterimut123",
 saldo: 59708,
 riwayat: [
 
+{ tanggal: "2025-07-15 21:17", jumlah: 3100, jenis: "masuk" },
 { tanggal: "2025-07-12 17:17", jumlah: 20000, jenis: "masuk" },
 { tanggal: "2025-07-12 17:17", jumlah: 15980, jenis: "masuk" },
 { tanggal: "2025-07-12 09:47", jumlah: 2500, jenis: "masuk" },
