6503f0918e8ff34c27b6003630111d13ee3216cf
tabunganku <updataback069@gmail.com>
2025-07-25 10:28:26 +0700

    Update zaen.js

--- zaen.js
+++ zaen.js
@@ -53,8 +53,9 @@ let users = JSON.parse(localStorage.getItem('users')) || [
         username: "2912244444",
         namaAsli: "Rahma",
         password: "rahma0106",
-        saldo: 145000,
+        saldo: 160000,
         riwayat: [
+        { tanggal: "2025-07-25 10:20", jumlah: 15000, jenis: "masuk" },        
         { tanggal: "2025-07-23 17:39", jumlah: 10000, jenis: "masuk" },
            { tanggal: "2025-07-22 17:55", jumlah: 5000, jenis: "masuk" },
             { tanggal: "2025-07-20 12:44", jumlah: 10000, jenis: "masuk" },
